f90e4461224d8cebfa6bb8c0988eac6694d6ab70
Bartosz Legięć <bartosz@legiec.io>
2022-02-05 21:54:20 +0100

    Update looks

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import type { GetServerSideProps } from 'next'
 import copy from 'copy-to-clipboard';
-import { Form, Button, Col, Container, Row, Toast, ToastContainer } from 'react-bootstrap'
+import { Form, Button, Col, Container, Row, Toast, ToastContainer, Navbar } from 'react-bootstrap'
 import { basicAuthCheck } from 'utils/basicAuthCheck'
 import { makeRequest } from 'utils/makeRequest'
 import { isValidSlug } from 'utils/validators/createUrl.utils'
@@ -61,69 +61,76 @@ const Admin = () => {
   }
 
   return (
-    <Container className="pt-5">
-      <ToastContainer className="p-3" position="top-center">
-        <Toast onClose={() => setShowToast(false)} show={showToast} delay={5000} autohide bg="success">
-          <Toast.Header>
-            <strong className="me-auto">An link created successfully!</strong>
-          </Toast.Header>
-          <Toast.Body className="text-white">
-            <div>
-              It&apos;s available at <a href={getUrl(origin, createdSlug)} className="text-white">{getUrl(origin, createdSlug)}</a>.
-            </div>
-            <div>
-              It was copied to clipboard.
-            </div>
-          </Toast.Body>
-        </Toast>
-      </ToastContainer>
-      <Row>
-        <Col>
-          <Form onSubmit={onSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Url</Form.Label>
-              <Form.Control
-                type="url"
-                placeholder="Enter url"
-                value={url}
-                onChange={onChange('url')}
-                disabled={isLoading}
-                required
-              />
-            </Form.Group>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>Link Shortener Admin</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container className="pt-3">
+        <ToastContainer className="p-3" position="top-center">
+          <Toast onClose={() => setShowToast(false)} show={showToast} delay={5000} autohide bg="success">
+            <Toast.Header>
+              <strong className="me-auto">An link created successfully!</strong>
+            </Toast.Header>
+            <Toast.Body className="text-white">
+              <div>
+                It&apos;s available at <a href={getUrl(origin, createdSlug)} className="text-white">{getUrl(origin, createdSlug)}</a>.
+              </div>
+              <div>
+                It was copied to clipboard.
+              </div>
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>
+        <Row>
+          <Col>
+            <Form onSubmit={onSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Url</Form.Label>
+                <Form.Control
+                  type="url"
+                  placeholder="Enter url"
+                  value={url}
+                  onChange={onChange('url')}
+                  disabled={isLoading}
+                  required
+                />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Custom Slug</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Custom Slug"
-                value={customSlug}
-                onChange={onChange('customSlug')}
-                disabled={isLoading}
-                isInvalid={!isSlugValid}
-              />
-              <Form.Control.Feedback type="invalid">
-                You can only use <span className="font-monospace">a-z A-Z 0-9 - . _</span> in the slug
-              </Form.Control.Feedback>
-              <Form.Text className="text-muted">
-                Enter a custom slug that will be used in the link.
-                {customSlug && (
-                  <div>
-                    The link will look like this: {getUrl(origin, customSlug)}
-                  </div>
-                )}
-              </Form.Text>
-            </Form.Group>
-            {error && <div className="text-danger mb-3">
-              {error}
-            </div>}
-            <Button variant="primary" type="submit" disabled={isLoading}>
-              {isLoading ? 'Loading...' : 'Add New Link'}
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Custom Slug</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Custom Slug"
+                  value={customSlug}
+                  onChange={onChange('customSlug')}
+                  disabled={isLoading}
+                  isInvalid={!isSlugValid}
+                />
+                <Form.Control.Feedback type="invalid">
+                  You can only use <span className="font-monospace">a-z A-Z 0-9 - . _</span> in the slug
+                </Form.Control.Feedback>
+                <Form.Text className="text-muted">
+                  Enter a custom slug that will be used in the link.
+                  {customSlug && (
+                    <div>
+                      The link will look like this: {getUrl(origin, customSlug)}
+                    </div>
+                  )}
+                </Form.Text>
+              </Form.Group>
+              {error && <div className="text-danger mb-3">
+                {error}
+              </div>}
+              <Button variant="primary" type="submit" disabled={isLoading}>
+                {isLoading ? 'Loading...' : 'Add New Link'}
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 
